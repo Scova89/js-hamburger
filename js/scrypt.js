f154f90475af99655fed4dae6058c45d3712b850
Francesco <scovascova@gmail.com>
2021-11-08 18:56:53 +0100

    menù hamburger

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,9 +1,17 @@
-const elemento = document.querySelector(".fas fa-bars")
+const elemento = document.querySelector(".fa-bars");
+const hmenu = document.querySelector(".hamburger-menu");
+const chiudi = document.querySelector(".fa-times");
 
 console.log(elemento)
 
-// .addEventListener("click", function() {
-//     document.querySelector(".hamburger-menu");
-//   });
+elemento.addEventListener('click', apriMenu);
 
-  
+function apriMenu(){
+    hmenu.classList.add('active');
+}
+
+chiudi.addEventListener('click', chiudiMenu);
+
+function chiudiMenu(){
+    hmenu.classList.remove('active');
+}
